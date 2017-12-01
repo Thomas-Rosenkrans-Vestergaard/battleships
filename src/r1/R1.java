c5532f3954e6377d73c903a28f44d1bbee1d2113
Thomas <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2017-12-01 10:54:13 +0100

    initial

---
+++ src/r1/R1.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package r1;
+
+import battleship.interfaces.BattleshipsPlayer;
+import tournament.player.PlayerFactory;
+
+/**
+ *
+ * @author Tobias Grundtvig
+ */
+public class R1 implements PlayerFactory<BattleshipsPlayer>
+{
+
+    public R1(){}
+    
+    
+    @Override
+    public BattleshipsPlayer getNewInstance()
+    {
+        return new RandomPlayer();
+    }
+
+    @Override
+    public String getID()
+    {
+        return "R1";
+    }
+
+    @Override
+    public String getName()
+    {
+        return "T3";
+    }
+
+    @Override
+    public String[] getAuthors()
+    {
+        String[] res = {"Thomas Rosenkrans Vestergaard, Thomas Fritzbøger, Tobias Leon Frehr"};
+        return res;
+    }
+    
+}
