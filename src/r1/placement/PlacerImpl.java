b7193f2c4339fb0e9c49aafb66463c5b91fa0430
thomasfritzboger <thomasfritzboger@10.50.130.10>
2017-12-04 13:03:56 +0100

    PlaceShip

--- src/r1/placement/PlacerImpl.java
+++ src/r1/placement/PlacerImpl.java
@@ -3,36 +3,64 @@ package r1.placement;
 import battleship.interfaces.Board;
 import battleship.interfaces.Fleet;
 import battleship.interfaces.Position;
+import battleship.interfaces.Ship;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import r1.HeatMap;
+import r1.HeatMapOutOfBoundsException;
 
-public class PlacerImpl implements Placer {
+public class PlacerImpl implements Placer
+{
+    private HeatMap incomingHeatMap;
 
     @Override
-    public void startMatch(int rounds, Fleet ships, int sizeX, int sizeY) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void startMatch(int rounds, Fleet ships, int sizeX, int sizeY)
+    {
+        this.incomingHeatMap =  new HeatMap(sizeX,sizeY);
     }
 
     @Override
-    public void placeShips(Fleet fleet, Board board) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void placeShips(Fleet fleet, Board board)
+    {
+
+        int numberOfShips = fleet.getNumberOfShips();
+
+        for (int i = 1; i <= numberOfShips; i++)
+        {
+            Ship ship = fleet.getShip(i);
+            ship.size();
+            Position pstn = new Position(0, i);
+            board.placeShip(pstn, ship, true);
+             
+            System.out.println(pstn.toString());
+        }
+        
     }
 
     @Override
-    public void incoming(Position pos) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void incoming(Position pos)
+    {
+        try
+        {
+            incomingHeatMap.put(pos);
+        } catch (HeatMapOutOfBoundsException ex)
+        {
+            Logger.getLogger(PlacerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
-    public void startRound(int round) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void startRound(int round)
+    {
     }
 
     @Override
-    public void endRound(int round, int points, int enemyPoints) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void endRound(int round, int points, int enemyPoints)
+    {
     }
 
     @Override
-    public void endMatch(int won, int lost, int draw) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void endMatch(int won, int lost, int draw)
+    {
     }
 }
